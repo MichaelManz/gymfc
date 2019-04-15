5c3be925b4679cffda7d23dafac25136a882b9b6
Michael Friedrich <mifriedr@adobe.com>
2019-04-15 18:36:17 +0200

    Build a docker image with gazebo and gymfc and run a demo
    Compiles mesa drivers to allow direct rendering from inside the docker image.
    Successfully tested on MacOS with Intel and Radeon graphics hardware but should also work on other hardware/os
    
    This can be used as starting point to build a docker image for headless training

--- tests/test_step_sim.py
+++ tests/test_step_sim.py
@@ -1,8 +1,9 @@
 import argparse
 import os
-from gymfc.envs.fc_env import FlightControlEnv 
+from gymfc.envs.fc_env import FlightControlEnv
 import numpy as np
 import time
+
 
 def step_sim(env, ac, delay=0):
     """ Evaluate an environment with the given policy """
@@ -16,10 +17,11 @@ def step_sim(env, ac, delay=0):
             break
     env.close()
 
+
 class Sim(FlightControlEnv):
 
-    def __init__(self, aircraft_config, config_filepath=None, max_sim_time=1):
-        super().__init__(aircraft_config, config_filepath=config_filepath)
+    def __init__(self, aircraft_config, config_filepath=None, max_sim_time=1, verbose=False):
+        super().__init__(aircraft_config, config_filepath=config_filepath, verbose=verbose)
         self.max_sim_time = max_sim_time
 
     def state(self):
@@ -37,20 +39,19 @@ class Sim(FlightControlEnv):
     def on_reset(self):
         pass
 
-if __name__ == "__main__":
 
+if __name__ == "__main__":
     parser = argparse.ArgumentParser("Step the simulator with the given motor values.")
     parser.add_argument('aircraftconfig', help="File path of the aircraft SDF.")
     parser.add_argument('value', nargs='+', type=float, help="List of control signals, one for each motor.")
     parser.add_argument('--gymfc-config', default=None, help="Option to override default GymFC configuration location.")
     parser.add_argument('--delay', default=0, type=float, help="Second delay betwee steps for debugging purposes.")
     parser.add_argument('--max-sim-time', default=1, type=float, help="Time in seconds the sim should run for.")
-
+    parser.add_argument('--debug', default=False, type=bool, help="Print debug information.")
 
     args = parser.parse_args()
 
-    env = Sim(args.aircraftconfig, config_filepath=args.gymfc_config, max_sim_time=args.max_sim_time)
+    env = Sim(args.aircraftconfig, config_filepath=args.gymfc_config, max_sim_time=args.max_sim_time,
+              verbose=args.debug)
     env.render()
     step_sim(env, np.array(args.value), delay=args.delay)
-
-
